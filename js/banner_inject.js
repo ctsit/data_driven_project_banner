56536ebae0cab9fd83a18771945a2fce2ef65684
Sai Pavan Kamma <saipavankamma@ufl.edu>
2024-07-16 22:38:18 -0400

    Check if the div is empty
    This change allows us to check if a div is empty. By doing this we prevent the issue of visibility of banners in other project pages.

--- js/banner_inject.js
+++ js/banner_inject.js
@@ -1,6 +1,9 @@
 $(document).ready(function() {
-
-  for (var i = 0; i < DDPB.DDPBs.length; ++i) {
-    $("#subheader").before(`<div class='project-banner' id='project-banner-${i}'> ${DDPB.DDPBs[i]}</div>`);
-  }
+    if (typeof DDPB !== 'undefined' && Array.isArray(DDPB.DDPBs)) {
+        for (var i = 0; i < DDPB.DDPBs.length; ++i) {
+            if (DDPB.DDPBs[i] !== "" && DDPB.DDPBs[i] !== "''") {
+                $("#subheader").before(`<div class='project-banner' id='project-banner-${i}'>${DDPB.DDPBs[i]}</div>`);
+            }
+        }
+    }
 });
